1efce37475ae1e9fd75c44a012bf58f5fb0887c6
hackjsw <110555343@qq.com>
2019-03-28 17:49:39 +0800

    django study

--- newsites/blog/views.py
+++ newsites/blog/views.py
@@ -4,7 +4,6 @@ from django.core.paginator import Paginator
 from django.conf import settings
 
 
-
 # Create your views here.
 def blog_list(request):
     blogs_all_list = Blog.objects.all()
@@ -36,8 +35,13 @@ def blog_list(request):
 
 
 def blog_detail(request, blog_pk):
-    context = {}
-    context['blog'] = get_object_or_404(Blog, pk=blog_pk)
+    blog = get_object_or_404(Blog, pk=blog_pk)
+    context = {
+        'previous_blog': Blog.objects.filter(created_time__gt=blog.created_time).last(),
+        'next_blog': Blog.objects.filter(created_time__lt=blog.created_time).first(),
+        'blog': blog,
+
+    }
     return render_to_response('blog/blog_detail.html', context)
 
 
@@ -67,14 +71,3 @@ def blogs_with_type(request, blog_type_pk):
         'blogs_all_list': blogs_all_list,
     }
     return render_to_response('blog/blogs_with_type.html', context)
-
-
-'''
-def blogs_with_type(request, blog_type_pk):
-    context = {}
-    blog_type = get_object_or_404(BlogType, pk=blog_type_pk)
-    context['blogs'] = Blog.objects.filter(blog_type=blog_type)
-    context['blog_type'] = blog_type
-    context['blog_types'] = BlogType.objects.all()
-    return render_to_response('blog/blogs_with_type.html', context)
-'''
