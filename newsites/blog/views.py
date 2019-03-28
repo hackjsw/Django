bd9f4d121408a70295d1fb9d183ab2ecd30d34a4
hackjsw <110555343@qq.com>
2019-03-28 19:27:54 +0800

    django study

--- newsites/blog/views.py
+++ newsites/blog/views.py
@@ -1,38 +1,45 @@
-from django.shortcuts import render_to_response, get_object_or_404
+from django.shortcuts import render_to_response, get_object_or_404,render
 from .models import Blog, BlogType
 from django.core.paginator import Paginator
 from django.conf import settings
 
 
 # Create your views here.
-def blog_list(request):
-    blogs_all_list = Blog.objects.all()
-    paginator = Paginator(blogs_all_list, settings.EACH_PAGE_OF_BLOGS)  # Ã¿10Æª½øĞĞ·ÖÒ³
-    page_num = request.GET.get('page', 1)  # »ñÈ¡Ò³Âë²ÎÊı(GETÇëÇó)
+def blog_list_common_data(request,blogs_all_list):
+    paginator = Paginator(blogs_all_list, settings.EACH_PAGE_OF_BLOGS)  # æ¯10ç¯‡è¿›è¡Œåˆ†é¡µ
+    page_num = request.GET.get('page', 1)  # è·å–é¡µç å‚æ•°(GETè¯·æ±‚)
     page_of_blogs = paginator.get_page(page_num)
-    # currentr_page_num = page_of_blogs.number #»ñÈ¡µ±Ç°Ò³Âë
+    # currentr_page_num = page_of_blogs.number #è·å–å½“å‰é¡µç 
     page_range = [x for x in range(int(page_num) - 2, int(page_num) + 3) if 0 < x <= paginator.num_pages]
     # page_of_blogs.object_list = panginator.get_page()
-    # ¼ÓÊ¡ÂÔÒ³Âë±ê¼Ç
+    # åŠ çœç•¥é¡µç æ ‡è®°
     if page_range[0] - 1 > 2:
         page_range.insert(0, '...')
     if paginator.num_pages - page_range[-1] > 2:
         page_range.append('...')
-    # ¼ÓÉÏÊ×Ò³ºÍÎ²Ò³
+    # åŠ ä¸Šé¦–é¡µå’Œå°¾é¡µ
     if page_range[0] != 1:
         page_range.insert(0, 1)
     if page_range[-1] != paginator.num_pages:
         page_range.append(paginator.num_pages)
 
-    context = {}
-    context['blogs'] = page_of_blogs.object_list
-    context['page_of_blogs'] = page_of_blogs
-    context['page_range'] = page_range
-    context['blog_types'] = BlogType.objects.all()
-    # ÁíÍâÒ»¸öÍ³¼ÆÊıÁ¿µÄ·½·¨
-    # context['blogs_count'] = Blog.objects.all().count()
-    return render_to_response('blog/blog_list.html', context)
+    context = {
+        'blogs': page_of_blogs.object_list,
+        'page_of_blogs': page_of_blogs,
+        'blog_types': BlogType.objects.all(),
+        'page_range': page_range,
+        'blogs_all_list': blogs_all_list,
+        'blog_dates': Blog.objects.dates('created_time', 'month', order='DESC'),
+    }
 
+    return context
+
+def blog_list(request):
+    blogs_all_list = Blog.objects.all()
+    context = blog_list_common_data(request,blogs_all_list)
+    # å¦å¤–ä¸€ä¸ªç»Ÿè®¡æ•°é‡çš„æ–¹æ³•
+    # context['blogs_count'] = Blog.objects.all().count()
+    return render(request,'blog/blog_list.html', context)
 
 def blog_detail(request, blog_pk):
     blog = get_object_or_404(Blog, pk=blog_pk)
@@ -42,32 +49,17 @@ def blog_detail(request, blog_pk):
         'blog': blog,
 
     }
-    return render_to_response('blog/blog_detail.html', context)
-
+    return render(request,'blog/blog_detail.html', context)
 
 def blogs_with_type(request, blog_type_pk):
     blog_type = get_object_or_404(BlogType, pk=blog_type_pk)
-    page_num = request.GET.get('page', 1)  # »ñÈ¡Ò³Âë²ÎÊı(GETÇëÇó)
     blogs_all_list = Blog.objects.filter(blog_type=blog_type)
-    paginator = Paginator(blogs_all_list, settings.EACH_PAGE_OF_BLOGS)
-    page_of_blogs = paginator.get_page(page_num)
-    page_range = [x for x in range(int(page_num) - 2, int(page_num) + 3) if 0 < x <= paginator.num_pages]
-    # ¼ÓÊ¡ÂÔÒ³Âë±ê¼Ç
-    if page_range[0] - 1 > 2:
-        page_range.insert(0, '...')
-    if paginator.num_pages - page_range[-1] > 2:
-        page_range.append('...')
-    # ¼ÓÉÏÊ×Ò³ºÍÎ²Ò³
-    if page_range[0] != 1:
-        page_range.insert(0, 1)
-    if page_range[-1] != paginator.num_pages:
-        page_range.append(paginator.num_pages)
-    context = {
-        'blogs': page_of_blogs.object_list,
-        'blog_type': blog_type,
-        'blog_types': BlogType.objects.all(),
-        'page_range': page_range,
-        'page_of_blogs': page_of_blogs,
-        'blogs_all_list': blogs_all_list,
-    }
-    return render_to_response('blog/blogs_with_type.html', context)
+    context = blog_list_common_data(request,blogs_all_list)
+    context['blog_type'] = blog_type
+    return render(request,'blog/blogs_with_type.html', context)
+
+def blogs_with_date(request, year, month):
+    blogs_all_list = Blog.objects.filter(created_time__year=year, created_time__month=month)
+    context = blog_list_common_data(request,blogs_all_list)
+    context['blogs_with_date'] = "{0}å¹´{1}æœˆ".format(year, month)
+    return render(request,'blog/blogs_with_date.html', context)
