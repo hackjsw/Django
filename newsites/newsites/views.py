39cd557c301c2ab3689f326b096bd4194d94a560
hackjsw <110555343@qq.com>
2019-04-02 15:24:28 +0800

    django study

--- newsites/newsites/views.py
+++ newsites/newsites/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render_to_response
+from django.contrib.contenttypes.models import ContentType
+from read_statistics.utils import get_seven_days_read_data,get_today_hot_data
+from blog.models import Blog
+
 
 def home(request):
-    context = {}
+    blog_content_type = ContentType.objects.get_for_model(Blog)
+    dates, read_nums = get_seven_days_read_data(blog_content_type)
+    today_hot_data = get_today_hot_data(blog_content_type)
+    context = {
+        'read_nums': read_nums,
+        'dates': dates,
+        'today_hot_day':today_hot_data,
+    }
     return render_to_response('home.html', context)
